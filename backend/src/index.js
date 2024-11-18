8a3057466894aec447321917d12b1970d932ac86
nirbhayVaghela92 <nirbhayVaghela92@gmail.com>
2024-11-18 18:16:29 +0530

    solving cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,14 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CORS_ORIGIN);  // Allow the specific origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 connectDB()
   .then(() => {
     app.on("error", (error) => {
